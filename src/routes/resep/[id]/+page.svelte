eecd1c0c18b85f7056956f211ec03dd5aa7c047f
Tertius van Niekerk <tertius.vanniekerk@protonmail.com>
2026-09-12 12:15:30 +0200

    Added fullscreen

--- src/routes/resep/[id]/+page.svelte
+++ src/routes/resep/[id]/+page.svelte
@@ -1,13 +1,14 @@
 <script>
   import DisplaySteps from "$lib/ui/DisplaySteps.svelte";
   import Icon from "$lib/ui/comps/Icon.svelte";
+  import Image from "./Image.svelte";
 
   const { data } = $props();
 
   const recipe = $derived(data.recipe);
 </script>
 
-<div class="px-4 pt-4 overflow-y-auto flex flex-col gap-6 h-full pb-50 scrollbar-none">
+<div class="pt-4 overflow-y-auto flex flex-col gap-6 h-full pb-50 scrollbar-none">
   {#if !recipe}
     <p class="text-center text-gray-500">Laai resep...</p>
   {:else}
@@ -17,9 +18,7 @@
 
     <div class="flex-wrap flex gap-1.5">
       {#each recipe.kategorieë as kategorie}
-        <span
-          class="inline-flex items-center px-2.5 py-0.5 rounded-lg font-medium bg-orange-100 text-orange-800 border border-orange-400"
-        >
+        <span class="bg-secondary-100 text-secondary-950 font-sans text-[12px] px-3 py-1 rounded-full font-bold">
           {kategorie}
         </span>
       {/each}
@@ -32,11 +31,11 @@
     {/if}
 
     {#if recipe?.foto}
-      <img src={recipe?.foto} alt={recipe?.naam} class="w-full h-64 object-cover rounded-lg shadow-md" />
+      <Image src={recipe?.foto} alt={recipe?.naam} />
     {/if}
 
     <div class="grid grid-cols-3 gap-5">
-      <div class="flex flex-col md:flex-row items-center gap-2">
+      <div class="p-2 flex flex-col md:flex-row items-center gap-2 bg-primary-50 border border-gray-300 rounded-lg">
         <div class="flex items-center gap-2">
           <Icon name="running-man" class="w-4 h-4 shrink-0" />
           <span>Werktyd:</span>
@@ -45,7 +44,7 @@
         <div class="font-semibold">{recipe?.tyd?.werk} min</div>
       </div>
 
-      <div class="flex flex-col md:flex-row items-center gap-2">
+      <div class="p-2 flex flex-col md:flex-row items-center gap-2 bg-primary-50 border border-gray-300 rounded-lg">
         <div class="flex items-center gap-2">
           <Icon name="clock" class="w-4 h-4 shrink-0" />
           <span>Wagtyd:</span>
@@ -53,7 +52,7 @@
         <div class="font-semibold">{recipe?.tyd.wag} min</div>
       </div>
 
-      <div class="flex flex-col md:flex-row items-center gap-2">
+      <div class="p-2 flex flex-col md:flex-row items-center gap-2 bg-primary-50 border border-gray-300 rounded-lg">
         <div class="flex items-center gap-2">
           <Icon name="people" class="w-4 h-4 shrink-0" />
           <span>Porsies:</span>
---
+++ src/routes/resep/[id]/Image.svelte
@@ -0,0 +1,18 @@
+<script>
+  const { src, alt } = $props();
+
+  let is_fullscreen = $state(false);
+</script>
+
+<button type="button" onclick={() => is_fullscreen = !is_fullscreen}>
+  <img {src} {alt} class="w-full h-64 object-cover rounded-lg shadow-md" />
+</button>
+
+{#if is_fullscreen}
+  <div class="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+    <button type="button" class="absolute top-4 right-4 text-white text-2xl" onclick={() => is_fullscreen = false}>
+      &times;
+    </button>
+    <img {src} {alt} class="max-w-full max-h-full object-contain" />
+  </div>
+{/if}
